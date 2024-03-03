9a6f48d03a77c79f9c5112aaccbf180ddd7d5cca
Thijs Boom <twboom@hotmail.nl>
2024-03-04 00:57:12 +0100

    Improvements

--- pong/js/game.js
+++ pong/js/game.js
@@ -21,13 +21,29 @@ const SESSION = {
 const CANVAS = document.getElementById('canvas');
 const CTX = CANVAS.getContext('2d');
 
+const CONSTANTS = {};
+function setConstants() {
+    CONSTANTS.PLAYER_LEFT_EDGE_LEFT = CONFIG.PLAYER_DISTANCE_TO_EDGE;
+    CONSTANTS.PLAYER_LEFT_EDGE_RIGHT = CONFIG.PLAYER_DISTANCE_TO_EDGE + CONFIG.PLAYER_WIDTH;
+    CONSTANTS.PLAYER_RIGHT_EDGE_LEFT = CANVAS.width - CONFIG.PLAYER_DISTANCE_TO_EDGE - CONFIG.PLAYER_WIDTH;
+    CONSTANTS.PLAYER_RIGHT_EDGE_RIGHT = CANVAS.width - CONFIG.PLAYER_DISTANCE_TO_EDGE;
+};
+
 class Player {
     constructor(xPos, controls) {
         this.score = 0;
         this.pos = CANVAS.height/2;
         this.controls = controls;
-        this.leftEdge = xPos == 'left' ? CONFIG.PLAYER_DISTANCE_TO_EDGE : CANVAS.width - CONFIG.PLAYER_DISTANCE_TO_EDGE - CONFIG.PLAYER_WIDTH/2;
+
+        this.edgeLeft = xPos == 'left' ? CONSTANTS.PLAYER_LEFT_EDGE_LEFT : CONSTANTS.PLAYER_RIGHT_EDGE_LEFT;
+        this.edgeRight = this.edgeLeft + CONFIG.PLAYER_WIDTH;
     };
+
+    get edgeTop() { return this.pos - CONFIG.PLAYER_HEIGHT/2; };
+    get edgeBottom() { return this.pos + CONFIG.PLAYER_HEIGHT/2; };
+
+    set edgeTop(value) { this.pos = value + CONFIG.PLAYER_HEIGHT/2 }
+    set edgeBottom(value) { this.pos = value - CONFIG.PLAYER_HEIGHT/2 }
 
     step() {
         if (this.controls) {
@@ -37,25 +53,23 @@ class Player {
     };
 
     move(direction) {
-        let newPos;
         switch (direction) {
             case 'up':
-                newPos = this.pos - CONFIG.PLAYER_SPEED;
-                if (newPos - CONFIG.PLAYER_HEIGHT/2 < 0) { newPos = CONFIG.PLAYER_HEIGHT/2; };
+                this.pos -= CONFIG.PLAYER_SPEED;
+                if (this.edgeTop < 0) { this.edgeTop = 0 };
                 break;
                 
             case 'down':
-                newPos = this.pos + CONFIG.PLAYER_SPEED;
-                if (newPos + CONFIG.PLAYER_HEIGHT/2 > CANVAS.height) { newPos = CANVAS.height - CONFIG.PLAYER_HEIGHT/2; };
+                this.pos += CONFIG.PLAYER_SPEED;
+                if (this.edgeBottom > CANVAS.height) { this.edgeBottom = CANVAS.height };
                 break;
         };
-        this.pos = newPos;
     };
 
 
     render() {
         CTX.beginPath();
-        CTX.rect(this.leftEdge, this.pos - CONFIG.PLAYER_HEIGHT/2, CONFIG.PLAYER_WIDTH, CONFIG.PLAYER_HEIGHT)
+        CTX.rect(this.edgeLeft, this.edgeTop, CONFIG.PLAYER_WIDTH, CONFIG.PLAYER_HEIGHT)
         CTX.fillStyle = 'white';
         CTX.fill();
     };
@@ -65,22 +79,36 @@ class Ball {
     constructor() {
         this.pos = {x: CANVAS.width/2, y: CANVAS.height/2};
         this.speed = {x: 5, y:5};
+
+        this.isScoring = false;
     };
 
-    step() {
-        // Update speed
-        if (this.pos.y - CONFIG.BALL_SIZE/2 < 0 || this.pos.y + CONFIG.BALL_SIZE/2 > CANVAS.height) { this.speed.y *= -1 };
+    get edgeLeft() { return this.pos.x - CONFIG.BALL_SIZE/2; };
+    get edgeRight() { return this.pos.x + CONFIG.BALL_SIZE/2; };
+    get edgeTop() { return this.pos.y - CONFIG.BALL_SIZE/2; };
+    get edgeBottom() { return this.pos.y + CONFIG.BALL_SIZE/2; };
 
+    step() {
+        if (this.edgeLeft <= 0) { scorePoint('RIGHT'); };
+        if (this.edgeLeft >= CANVAS.width) { scorePoint('LEFT'); };
+
+        // Top/Bottom edge detection
+        if (this.edgeTop < 0 || this.edgeBottom > CANVAS.height) { this.speed.y *= -1 };
+
+        // Player hit detection
         if (
-            (this.pos.x - CONFIG.BALL_SIZE/2 < CONFIG.PLAYER_DISTANCE_TO_EDGE + CONFIG.PLAYER_WIDTH &&
+            !this.isScoring &&
+            (this.edgeLeft <= CONSTANTS.PLAYER_LEFT_EDGE_RIGHT &&
             (
-                this.pos.y > SESSION.PLAYERS.LEFT.pos - CONFIG.PLAYER_HEIGHT/2 &&
-                this.pos.y < SESSION.PLAYERS.LEFT.pos + CONFIG.PLAYER_HEIGHT/2
+                this.pos.y > SESSION.PLAYERS.LEFT.edgeTop &&
+                this.pos.y < SESSION.PLAYERS.LEFT.edgeBottom
+                // true
             )) || 
-            (this.pos.x + CONFIG.BALL_SIZE/2 > CANVAS.width - CONFIG.PLAYER_DISTANCE_TO_EDGE - CONFIG.PLAYER_WIDTH &&
+            (this.edgeRight >= CONSTANTS.PLAYER_RIGHT_EDGE_LEFT &&
             (
-                this.pos.y > SESSION.PLAYERS.RIGHT.pos - CONFIG.PLAYER_HEIGHT/2 &&
-                this.pos.y < SESSION.PLAYERS.RIGHT.pos + CONFIG.PLAYER_HEIGHT/2
+                this.pos.y > SESSION.PLAYERS.RIGHT.edgeTop &&
+                this.pos.y < SESSION.PLAYERS.RIGHT.edgeBottom
+                // true
             ))
         ) { this.speed.x *= -1 };
 
@@ -89,13 +117,13 @@ class Ball {
         this.pos.y += this.speed.y;
 
         // Check for scores
-        if (this.pos.x - CONFIG.BALL_SIZE/2 <= 0) { scorePoint('RIGHT'); };
-        if (this.pos.x + CONFIG.BALL_SIZE/2 >= CANVAS.width) { scorePoint('LEFT'); };
+        if (this.edgeLeft < CONSTANTS.PLAYER_LEFT_EDGE_LEFT) { this.isScoring = true; };
+        if (this.edgeRight > CONSTANTS.PLAYER_RIGHT_EDGE_RIGHT) { this.isScoring = true; };
     }
 
     render() {
         CTX.beginPath();
-        CTX.rect(this.pos.x - CONFIG.BALL_SIZE/2, this.pos.y - CONFIG.BALL_SIZE, CONFIG.BALL_SIZE, CONFIG.BALL_SIZE);
+        CTX.rect(this.edgeLeft, this.edgeTop, CONFIG.BALL_SIZE, CONFIG.BALL_SIZE);
         CTX.fillStyle = 'white';
         CTX.fill();
     };
@@ -123,6 +151,7 @@ function scorePoint(player) {
     SESSION.BALL.speed.x *= -1;
     SESSION.BALL.pos = {x: CANVAS.width/2, y: CANVAS.height/2};
     SESSION.PLAYERS[player].score++;
+    SESSION.BALL.isScoring = false;
 }
 
 function frame() {
@@ -163,13 +192,15 @@ function init() {
     // Set canvas size
     resizeCanvas();
 
+    setConstants();
+
     // Add the players
     SESSION.PLAYERS.LEFT = new Player('left', {up: 'KeyW', down: 'KeyS'}); 
-    SESSION.PLAYERS.RIGHT = new Player('right');
+    SESSION.PLAYERS.RIGHT = new Player('right', {up: 'ArrowUp', down: 'ArrowDown'});
 
     // Add agent for player RIGHT
-    // new Agent(SESSION.PLAYERS.LEFT);
-    new Agent(SESSION.PLAYERS.RIGHT);
+    new Agent(SESSION.PLAYERS.LEFT);
+    // new Agent(SESSION.PLAYERS.RIGHT);
 
     // Add the ball
     SESSION.BALL = new Ball();
